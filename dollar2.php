c4c3520e6244ca5596363cd5eb9f1cc226c032a8
tatabharathyadav <130755163+tatabharathyadav@users.noreply.github.com>
2023-04-18 10:48:36 +0530

    Add files via upload

---
+++ dollar2.php
@@ -0,0 +1,7 @@
+<?php
+echo "hello";
+$a=10;
+$b=20;
+$c=$a+$b;
+echo "the sum is",$c;
+?>
---
+++ loginform2.php
@@ -0,0 +1,6 @@
+<?php
+$a=$_POST["name"];
+$b=$_POST["email"];
+echo "Welcome" . " ".$a."</br>";
+echo "Your email address is:"." ".$b."</br>";
+?>
