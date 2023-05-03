eab99eadb61f38c511481df247bda2e10dc30785
tatabharathyadav <130755163+tatabharathyadav@users.noreply.github.com>
2023-05-03 22:50:38 +0530

    Add files via upload
    my wt

---
+++ my21B91A54H2/insert.php
@@ -0,0 +1,30 @@
+<?php
+$server="127.0.0.1";
+    $user="BHARATH";
+    $pass="BHARATH";
+    $dbname="bharath";
+    $con =mysqli_connect($server,$user,$pass,$dbname);
+    if(!$con)
+    {
+        die("Error : :  ".mysqli_connect_error());}
+if(isset($_POST['uname']))
+{
+$uname=$_POST['uname'];
+$pass=$_POST['pass'];
+$email=$_POST['email'];
+$phno=(float)$_POST['phone'];
+$query = "INSERT INTO registration VALUES ('$uname', '$pass', '$email', '$phno')";
+$res = mysqli_query($con,$query);
+if($res)
+{
+$script = "<script>alert('User Registration Successful');";
+$script .= "document.writeln('<center><h4><u>User Details</u></h1>');";
+$script .= "document.writeln('Your username is:<b> $uname</b><br/>');";
+$script .= "document.writeln('Your password is:<b> $pass</b></center>');</script>";
+echo $script;
+}
+else
+echo "<script>alert('User Registration Failed'); window.location = 'registration.html';
+</script>"; 
+} 
+?>
---
+++ my21B91A54H2/validating.php
@@ -0,0 +1,23 @@
+<?php 
+$server="127.0.0.1";
+    $user="BHARATH";
+    $pass="BHARATH";
+    $dbname="yadav";
+    $con =mysqli_connect($server,$user,$pass,$dbname);
+    if(!$con)
+    {
+        die("Error : :  ".mysqli_connect_error());}
+if(isset($_POST['uname']))
+{
+$uname=$_POST['uname'];
+$pass=$_POST['pass'];
+$query = "select * from registarting where uname='$uname' and pass ='$pass'";
+$res = mysqli_query($con,$query);
+$count=mysqli_num_rows($res);
+if($count)
+echo "<script>alert('Welcome $uname');</script>";
+else
+echo "<br/><br/><br/><h2 align='center'>Invalid Username/Password/Try 
+again!!!!!</h2>";
+}
+?>
